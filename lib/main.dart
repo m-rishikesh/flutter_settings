ff46c81eb4112f954351330745a5f59363756d73
saurya19 <kumarsaurya431@gmail.com>
2025-05-30 23:35:04 +0530

    commited_saurya

--- lib/main.dart
+++ lib/main.dart
@@ -67,3 +67,78 @@ class _SettingsHomeState extends State<SettingsHome> {
               );
             },
           ),
+          ListTile(
+            leading: Icon(Icons.battery_2_bar_sharp),
+            title: Text('Battery'),
+            subtitle: Text("54% - Untill 6:15 am"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.storage),
+            title: Text('Storage'),
+            subtitle: Text("22% used - 99.49 GB free"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.location_pin),
+            title: Text('Location'),
+            subtitle: Text("On - 17 apps have access to location"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.accessibility),
+            title: Text('Accessibility'),
+            subtitle: Text("Display,interaction,audio"),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.apps),
+            title: Text('Apps'),
+            subtitle: Text("Recent apps, default apps"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.password),
+            title: Text('Passwords,passkeys & accounts'),
+            subtitle: Text("Suggestions for sign-in & auto-fill"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.security),
+            title: Text('Security & privacy'),
+            subtitle: Text("App security, device lock, permission"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Saftey & emergency'),
+            subtitle: Text("Emergency SOS, medical info, alerts"),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.health_and_safety),
+            title: Text('Digital Wellbeing and parental controls'),
+            subtitle: Text("Screen Time,App Timers,bedtime schedules"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.update),
+            title: Text('System Updates'),
+            subtitle: Text("Update to the latest software"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.feed),
+            title: Text('Rating & Feedback'),
+            subtitle: Text("Send suggestions and rate your device"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.help_outline),
+            title: Text('Help'),
+            subtitle: Text("Use your phone to fix any issues"),
+            onTap: () {},
+          ),
+          
